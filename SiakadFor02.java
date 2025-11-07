0b10167236ecbcede2e43a08898b7a2266ba52c0
AkhdanNajam <asilahasna1@gmail.com>
2025-11-07 09:07:38 +0700

    akhir

--- SiakadFor02.java
+++ SiakadFor02.java
@@ -2,21 +2,20 @@ import java.util.Scanner;
 public class SiakadFor02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Masukkan jumlah mahasiswa: ");
-        int jumlahMahasiswa = scanner.nextInt();
-
-        int totalNilai = 0;
-
-        for (int i = 1; i <= jumlahMahasiswa; i++) {
+        double nilai, tertinggi = 0, terendah = 100;
+        for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
-            int nilai = scanner.nextInt();
-            totalNilai += nilai;
+            nilai = scanner.nextDouble();
+
+            if (nilai > tertinggi) {
+                tertinggi = nilai;
+            }
+            if (nilai < terendah) {
+                terendah = nilai;
+            }
         }
-
-        double rataRata = (double) totalNilai / jumlahMahasiswa;
-        System.out.println("Rata-rata nilai mahasiswa: " + rataRata);
-
+        System.out.println("Nilai tertinggi: " + tertinggi);
+        System.out.println("Nilai terendah: " + terendah);
         scanner.close();
     }
 }   
