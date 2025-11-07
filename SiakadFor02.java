518c5d53f4e5d3cd5045eeb74681220c938576cd
AkhdanNajam <asilahasna1@gmail.com>
2025-11-07 09:35:25 +0700

    modifikasi

--- SiakadFor02.java
+++ SiakadFor02.java
@@ -3,6 +3,8 @@ public class SiakadFor02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus  = 0, tidakLulus = 0;
+        int bataslulus = 60;
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
             nilai = scanner.nextDouble();
@@ -13,9 +15,18 @@ public class SiakadFor02 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if (nilai >= bataslulus) {
+                lulus++;
+            } else {
+                tidakLulus++;
+            }
         }
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiswa yang lulus: " + lulus);
+        System.out.println("Jumlah mahasiswa yang tidak lulus: " + tidakLulus);
+        
+    
         scanner.close();
     }
 }   
